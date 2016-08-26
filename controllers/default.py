2f4d96483d84de7de5c5670a265615b58131cdd8
Jonathan Karimian <Jonathan.Karimian@sa.catapult.org.uk>
2016-08-26 13:45:45 +0100

    Altered functest module name to excelHandling

--- controllers/default.py
+++ controllers/default.py
@@ -7,7 +7,7 @@
 # - user is required for authentication and authorization
 # - download is for downloading files uploaded in the db (does streaming)
 # -------------------------------------------------------------------------
-from functest import *
+from excelHandling import *
 import os
 
 def index():
