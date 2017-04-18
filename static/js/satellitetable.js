98d4dabc8807abb3ac6e8c03e87d4e88c67c2d64
Simon Andersson <simon.andersson@sa.catapult.org.uk>
2017-04-18 09:33:57 +0100

    both tables working on tabs

--- static/js/satellitetable.js
+++ static/js/satellitetable.js
@@ -2,37 +2,32 @@ var satellitetable = $('script[src*=satellitetable]');
 var satjson = satellitetable.attr('jsons');
 var editableGridsat = null;
 
-function loadJSONsat()
-{
-	editableGridsat = new window.EditableGrid("satgrid", {
 
-		// called when the XML has been fully loaded
-		tableLoaded: function() {
+editableGridsat = new window.EditableGrid("satgrid", {
 
-			// display a message
+	// called when the XML has been fully loaded
+	tableLoaded: function() {
 
-			// renderer for the action column
-			this.setCellRenderer("action", new CellRenderer({render: function(cell, value) {
-			cell.innerHTML = "<a onclick=\"if (confirm('Are you sure you want to delete this ? ')) editableGridsat.remove(" + cell.rowIndex + ");\" style=\"cursor:pointer\">" +
-							 "<img src=\"/linkbudgetweb/static/images/delete.png\" border=\"0\" alt=\"delete\" title=\"delete\"/></a>";
+		// display a message
 
-			}}));
+		// renderer for the action column
+		this.setCellRenderer("action", new CellRenderer({render: function(cell, value) {
+		cell.innerHTML = "<a onclick=\"if (confirm('Are you sure you want to delete this ? ')) editableGridsat.remove(" + cell.rowIndex + ");\" style=\"cursor:pointer\">" +
+						 "<img src=\"/linkbudgetweb/static/images/delete.png\" border=\"0\" alt=\"delete\" title=\"delete\"/></a>";
 
-			// render the grid
-			this.renderGrid("satellite_tablecontent", "testgrid");
-		},
+		}}));
 
-		// called when some value has been modified: we display a message
-		modelChanged: function(rowIdx, colIdx, oldValue, newValue, row) { $.ajax({
-          type: "POST",
-          url: "/lbController/ajax_to_db",
-          data: "array="+JSON.stringify({"table":"SAT", "columnname":editableGridsat.columns[colIdx].name, "value":newValue, "rowid":row,})
-       }).done(function( msg ) { }); }
-	});
+		// render the grid
+		this.renderGrid("satellite_tablecontent", "testgrid");
+	},
 
-	// load XML file
-	editableGridsat.loadJSON(satjson);
-}
+	// called when some value has been modified: we display a message
+	modelChanged: function(rowIdx, colIdx, oldValue, newValue, row) { $.ajax({
+        type: "POST",
+        url: "/lbController/ajax_to_db",
+        data: "array="+JSON.stringify({"table":"SAT", "columnname":editableGridsat.columns[colIdx].name, "value":newValue, "rowid":row,})
+     }).done(function( msg ) { }); }
+});
 
-// start when window is loaded
-loadJSONsat();
+// load XML file
+editableGridsat.loadJSON(satjson);
--- static/js/simpletable.js
+++ static/js/simpletable.js
@@ -1,37 +1,31 @@
 var simpletable = $('script[src*=simpletable]');
 var json = simpletable.attr('json');
-var editableGrid = null;
+var editableGridtrsp = null;
 
-function loadJSON()
-{
-	editableGridtrsp = new window.EditableGrid("trspgrid", {
-		// called when the XML has been fully loaded
-		tableLoaded: function() {
+editableGridtrsp = new window.EditableGrid("trspgrid", {
+	// called when the XML has been fully loaded
+	tableLoaded: function() {
 
-			// display a message
+		// display a message
 
-			// renderer for the action column
-			this.setCellRenderer("action", new CellRenderer({render: function(cell, value) {
-			cell.innerHTML = "<a onclick=\"if (confirm('Are you sure you want to delete this ? ')) editableGrid.remove(" + cell.rowIndex + ");\" style=\"cursor:pointer\">" +
-							 "<img src=\"/linkbudgetweb/static/images/delete.png\" border=\"0\" alt=\"delete\" title=\"delete\"/></a>";
+		// renderer for the action column
+		this.setCellRenderer("action", new CellRenderer({render: function(cell, value) {
+		cell.innerHTML = "<a onclick=\"if (confirm('Are you sure you want to delete this ? ')) editableGrid.remove(" + cell.rowIndex + ");\" style=\"cursor:pointer\">" +
+						 "<img src=\"/linkbudgetweb/static/images/delete.png\" border=\"0\" alt=\"delete\" title=\"delete\"/></a>";
 
-			}}));
+		}}));
 
-			// render the grid
-			this.renderGrid("trsp_tablecontent", "testgrid");
-		},
+		// render the grid
+		this.renderGrid("trsp_tablecontent", "testgrid");
+	},
 
-		// called when some value has been modified: we display a message
-		modelChanged: function(rowIdx, colIdx, oldValue, newValue, row) { $.ajax({
-          type: "POST",
-          url: "/lbController/ajax_to_db",
-          data: "array="+JSON.stringify({"table":"TRSP", "columnname":editableGridtrsp.columns[colIdx].name, "value":newValue, "rowid":row,})
-       }).done(function( msg ) { }); }
-	});
+	// called when some value has been modified: we display a message
+	modelChanged: function(rowIdx, colIdx, oldValue, newValue, row) { $.ajax({
+        type: "POST",
+        url: "/lbController/ajax_to_db",
+        data: "array="+JSON.stringify({"table":"TRSP", "columnname":editableGridtrsp.columns[colIdx].name, "value":newValue, "rowid":row,})
+     }).done(function( msg ) { }); }
+});
 
-	// load XML file
-	editableGridtrsp.loadJSON(json);
-}
-
-// start when window is loaded
-window.onload = loadJSON;
+// load XML file
+editableGridtrsp.loadJSON(json);
