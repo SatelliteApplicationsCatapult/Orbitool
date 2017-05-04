40a83c927e360c25c50b2d6656c05a747b722046
Simon Andersson <git@snomi.co.uk>
2017-05-04 21:39:52 +0100

    select-> scenario, remove serch button

--- models/menu.py
+++ models/menu.py
@@ -31,7 +31,7 @@ response.google_analytics_id = None
 
 response.menu = [
     (T('Home'), False, URL('lbController', 'index'), []),
-    (T('Select'), False, URL('lbController', 'select')),
+    (T('Scenarios'), False, URL('lbController', 'select')),
     (T('About'), False, URL('lbController', 'about')),
     (T('Admin'), False, URL('admin', 'default', 'site')),
 
