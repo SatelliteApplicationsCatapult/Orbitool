5a3ce1271922b092d0b3e7b3e9c67aac625012f7
SA-CATAPULT\Yibiao.Li <yibiao.li@sa.catapult.org.uk>
2018-01-23 10:37:46 +0000

    Simplify the default controller

--- controllers/default.py
+++ controllers/default.py
@@ -49,21 +49,6 @@ def about():
     """ About page """
     return dict(message=T('About'))
 
-# ---- API (example) -----
-@auth.requires_login()
-def api_get_user_email():
-    if not request.env.request_method == 'GET': raise HTTP(403)
-    return response.json({'status':'success', 'email':auth.user.email})
-
-# ---- Smart Grid (example) -----
-@auth.requires_membership('admin') # can only be accessed by members of admin groupd
-def grid():
-    response.view = 'generic.html' # use a generic view
-    tablename = request.args(0)
-    if not tablename in db.tables: raise HTTP(403)
-    grid = SQLFORM.smartgrid(db[tablename], args=[tablename], deletable=False, editable=False)
-    return dict(grid=grid)
-
 def user():
      
     if 'login' in request.args:
@@ -79,45 +64,6 @@ def user():
 
 def register():
     return dict(form=auth.register())
-
-#@auth.requires_membership("admin") # uncomment to enable security 
-def list_users():
-    btn = lambda row: A("Edit", _href=URL('manage_user', args=row.auth_user.id))
-    db.auth_user.edit = Field.Virtual(btn)
-    rows = db(db.auth_user).select()
-    headers = ["ID", "Name", "Last Name", "Email", "Edit"]
-    fields = ['id', 'first_name', 'last_name', "email", "edit"]
-    table = TABLE(THEAD(TR(*[B(header) for header in headers])),
-                  TBODY(*[TR(*[TD(row[field]) for field in fields]) \
-                        for row in rows]))
-    table["_class"] = "table table-striped table-bordered table-condensed"
-    return dict(table=table)
-
-#@auth.requires_membership("admin") # uncomment to enable security 
-def manage_user():
-    user_id = request.args(0) or redirect(URL('list_users'))
-    form = SQLFORM(db.auth_user, user_id).process()
-    membership_panel = LOAD(request.controller,
-                            'manage_membership.html',
-                             args=[user_id],
-                             ajax=True)
-    return dict(form=form,membership_panel=membership_panel)
-
-#@auth.requires_membership("admin") # uncomment to enable security 
-def manage_membership():
-    user_id = request.args(0) or redirect(URL('list_users'))
-    db.auth_membership.user_id.default = int(user_id)
-    db.auth_membership.user_id.writable = False
-    form = SQLFORM.grid(db.auth_membership.user_id == user_id,
-                       args=[user_id],
-                       searchable=False,
-                       deletable=False,
-                       details=False,
-                       selectable=False,
-                       csv=False,
-                       user_signature=False)
-    return form
-
 
 @auth.requires_login()
 def select():
